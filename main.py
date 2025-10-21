9709f8db780bde571b38c36d002b0d0786097372
Awais-Mindrind <m.awais@mindrind.com>
2025-10-21 16:15:59 +0500

    basic webhooks setup and ci piplies added

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from smartreply-clone!")
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+import json
+load_dotenv()
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+VERIFY_TOKEN = os.getenv("META_API_TOKEN")  # must match exactly what you entered in Meta dashboard
+
+@app.get("/webhook")
+async def verify_webhook(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return int(challenge)  # ✅ VERY IMPORTANT — return raw challenge
+    else:
+        return "Verification token mismatch", 403
+
+@app.post("/webhook")
+async def webhook_events(request: Request):
+    data = await request.json()
+    filename = data.get("field", "webhook")
+    with open(f"{filename}.json", "w") as f:
+        json.dump(data, f, indent=4)
+    print("🔔 NEW WEBHOOK EVENT:", data)
+    return {"status": "received"}
